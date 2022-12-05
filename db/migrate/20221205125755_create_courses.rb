d82da4056126108e74671c140f4e47efa952e491
ruthwekesa <wekesaruth569@gmail.com>
2022-12-05 16:48:19 +0300

    updating  course migration

--- db/migrate/20221205125755_create_courses.rb
+++ db/migrate/20221205125755_create_courses.rb
@@ -1,7 +1,10 @@
 class CreateCourses < ActiveRecord::Migration[7.0]
   def change
     create_table :courses do |t|
-
+      t.string :course_name
+      t.integer :school_yr
+      t.string :course_desc
+      
       t.timestamps
     end
   end
