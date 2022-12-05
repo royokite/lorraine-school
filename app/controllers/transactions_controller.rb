20dc45dc89f2796d1703f94687c4531f051ea08d
ruthwekesa <wekesaruth569@gmail.com>
2022-12-06 01:40:07 +0300

    added to transactions controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,8 @@
 class TransactionsController < ApplicationController
 
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    
     def index
         transaction = Transaction.all
         render json: transaction
@@ -10,5 +11,34 @@ class TransactionsController < ApplicationController
     def show
         transaction = Transaction.find_by(params[:id])
         render json: transaction
+    end
+    
+    def create
+        transaction = Transaction.create!(transaction_params)
+        render json: transaction, status: :created
+    end
+
+    def destroy
+        transaction = Transaction.find(params[:id])
+        transaction.destroy
+        head :no_content
+    end
+
+    private
+
+    def transaction_params
+        params.permit(:trans_id, :trans_name, :stud_id, :trans_date)
+    end
+
+    def find_ transaction
+        Transaction.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "Transaction not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
     end
 end
