ab63f11cb1e56e99478a6621a8625b1a0f9295b4
ruthwekesa <wekesaruth569@gmail.com>
2022-12-05 16:39:33 +0300

    subject model

---
+++ app/models/subject.rb
@@ -0,0 +1,4 @@
+class Subject < ApplicationRecord
+    belongs_to :course
+    belongs_to :student, through: :schedule
+end
---
+++ db/migrate/20221205133604_create_subjects.rb
@@ -0,0 +1,8 @@
+class CreateSubjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subjects do |t|
+      t.string :sub_name
+      t.timestamps
+    end
+  end
+end
