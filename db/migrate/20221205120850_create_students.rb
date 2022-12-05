bd6b5e9167d05fa969d4374620539439852d04d3
ruthwekesa <wekesaruth569@gmail.com>
2022-12-05 15:34:54 +0300

    student migration

--- db/migrate/20221205120850_create_students.rb
+++ db/migrate/20221205120850_create_students.rb
@@ -1,7 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :age
+      t.boolean :gender
+      t.integer :grade
+      t.string :email
       t.timestamps
     end
   end
