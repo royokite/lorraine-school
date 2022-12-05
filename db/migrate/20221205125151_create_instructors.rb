f3f69ec8a8557ad6892429f273f9c0097936b890
ruthwekesa <wekesaruth569@gmail.com>
2022-12-05 15:54:51 +0300

    instructor's attributes

--- db/migrate/20221205125151_create_instructors.rb
+++ db/migrate/20221205125151_create_instructors.rb
@@ -1,12 +1,12 @@
-class CreateStudents < ActiveRecord::Migration[7.0]
+class CreateInstructors < ActiveRecord::Migration[7.0]
   def change
-    create_table :students do |t|
+    create_table :instructors do |t|
       t.string :first_name
       t.string :last_name
       t.string :age
       t.boolean :gender
       t.integer :grade
-      t.string :stud_email
+      t.string :ins_email
       t.timestamps
     end
   end
