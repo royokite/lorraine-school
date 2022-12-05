8b4027e9807192c5cc1fe7a5a9343a7bcae17122
Angela <chepkoechangela514@gmail.com>
2022-12-05 21:19:46 +0300

    Students model done

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem "puma", "~> 5.0"
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
+gem 'active_model_serializers', '0.9.3'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem "rack-cors"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :student, only: [:index, :show, :create, :update, :destroy]
+  resources :students, only: [:index, :create, :show, :update, :destroy]
 end
+
