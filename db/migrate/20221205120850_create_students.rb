152ddf2ae88b0ccc7c34aa4aceb569ecc2906a76
ruthwekesa <wekesaruth569@gmail.com>
2022-12-06 00:53:05 +0300

    added to controllers

--- db/migrate/20221205120850_create_students.rb
+++ db/migrate/20221205120850_create_students.rb
@@ -1,14 +1,14 @@
 class CreateStudents < ActiveRecord::Migration[7.0]
   def change
     create_table :students do |t|
-      t.integer :stud_id
-      t.string :first_name
-      t.string :last_name
-      t.string :age
-      t.boolean :gender
-      t.integer :grade
-      t.string :stud_email
-      t.integer :ins_id
+      t.string :firstname
+      t.string :lastname
+      t.integer :age
+      t.integer :contact
+      t.string :gender
+      t.string :email
+      t.string :password
+
       t.timestamps
     end
   end
