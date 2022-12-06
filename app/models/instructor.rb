cf361ea7aa902f1764b049508d0bdd798c3b97b7
Titus Mumo <titusmumo@gmail.com>
2022-12-06 12:04:46 +0300

    relationships

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,4 +1,6 @@
 class Instructor < ApplicationRecord
     has_many :students
-    has_many :courses, through: :students
+    has_many :schedules
+    has_many :courses, through: :schedules
+    has_many :students, through: :schedules
 end
