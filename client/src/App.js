f71986319dcc63df80891747a3043806e4368465
RoyOkite <royokite@gmail.com>
2022-12-07 17:07:32 +0300

    NavBar CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import "./index.css"
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Login />
+      <NavBar />
     </BrowserRouter>
   );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,28 +2,33 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+    let activeStyle = {
+        backgroundColor: "rgb(49, 46, 129)", 
+    }; 
+
     const imageStyle = {
-        height: "8rem", 
-        width: "10rem", 
+        height: "4rem", 
+        width: "5rem", 
         alignSelf: "center", 
         display: "block", 
         marginLeft: "auto", 
         marginRight: "auto", 
-        marginBottom: "2rem",
-        paddingTop: "1rem",
-    }
+        // marginBottom: "2rem",
+        // paddingTop: "1rem",
+    };
 
     return (
-        <nav>
-            <article>
+        <nav className="bg-gray-800  mt-0 mb-3 sticky w-full z-10 top-0 flex flex-wrap text-slate-200">
+            <article className="flex-none">
                 <NavLink to="/">
                     <img src="./images/lorraine-logo.png" alt="school logo" style={imageStyle}/>
                 </NavLink>
             </article>
-            <article>
-                <NavLink to="/">Dashboard</NavLink>
-                <NavLink to="/instructors">Instructors</NavLink>
-                <NavLink to="/students">Students</NavLink>
+            <article className="mt-5 flex-none ml-auto">
+                <NavLink to="/" exact="true" end className="bg-gray-800 m-2 p-2 hover:bg-indigo-500" style={({ isActive }) => isActive ? activeStyle : undefined }>Dashboard</NavLink>
+                <NavLink to="/instructors" exact="true" className="bg-gray-800 m-2 p-2 hover:bg-indigo-500" style={({ isActive }) => isActive ? activeStyle : undefined }>Instructors</NavLink>
+                <NavLink to="/students" exact="true" className="bg-gray-800 m-2 p-2 hover:bg-indigo-500" style={({ isActive }) => isActive ? activeStyle : undefined }>Students</NavLink>
+                <NavLink to="/courses" exact="true" className="bg-gray-800 m-2 p-2 hover:bg-indigo-500" style={({ isActive }) => isActive ? activeStyle : undefined }>Courses</NavLink>
             </article>
         </nav>
     );
